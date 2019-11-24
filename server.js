cf995aae488279e818c2df18d71ed19bca605783
dyl trimble <dyltrimble12@gmail.com>
2019-11-23 20:42:17 -0500

    added saved handlebars and updated my templates, now using routes outside of server as server routes were giving me a ton of issues

--- server.js
+++ server.js
@@ -1,128 +1,44 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const mongoose = require("mongoose"); 
-const bodyParser = require("body-parser");
-const request = require('request');
-const logger = require("morgan"); 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-const axios = require("axios");
-const cherio = require("cherio");
+var express = require("express");
+var exphbs = require("express-handlebars");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-// Require all models
-const db = require("./models");
 
-const PORT = process.env.PORT || 3000;
 
-const app = express();
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
-// Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
-// Use express.static to serve the public folder as a static directory
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
 
 // Connect to the Mongo DB
-const MONGODB_URI = process.env.MONGOLAB_AQUA_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-
-app.get("/", (req, res) => {
-  
-  res.render("index");
-});
-
-app.get("/scrape", (req, res) => {
-
-  axios.get("https://quillette.com").then((response) => {
-
-  const $ = cherio.load(response.data); 
-
-  $("h3.entry-title, h2.entry-title").each(function(i, element) { 
-    
-    const result = {}; 
-    
-    result.title = $(this) 
-    .text();
-    result.link = $(this)
-    .children("a")
-    .attr("href");
-    result.summary = $(this)
-    .siblings("p.summary")
-    .text();
-    // Create a new Article using the 'result' object built from scraping
-    db.Article.create(result)
-      .then((dbArticle) => {
-        // View the added result in the console
-      })
-      .catch((err) => res.json(err));
-  });
-  // If we were successful scraping and save an Article, send a message to the client
-  // res.send("Scrape Complete");
-  res.redirect("/articles");
-  
-  });
-});
-
-// Route for getting all Articles from the db
-app.get("/articles", (req, res) => {
-  // Grab every doc in the articles collection
-  db.Article.find({})
-  .then((dbArticle) => {
-    // if find articles, send them to the client
-    res.render("index", { dbArticle } )
-  })
-    .catch((err) => {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-  })
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", (req, res) => {
-  // query that finds the matching one in our db
-  db.Article.findOne({ _id: req.params.id })
-  // ..and populate all of the notes associated with it
-  .populate("note")
-  .then((dbArticle) => {
-    // If we were able to successfully find an Article with the given id, send it back to the client
-    res.json(dbArticle);
-  })
-  .catch((err) => {
-    res.json(err);
-  })
-})
-
-// Route for saving or updating a given Article's associated Note
-app.post("/articles/:id", (req, res) => {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-  .then((dbNote) => db.Article.findOneAndUpdate({_id: req.params.id }, { note: dbNote._id}, { new: true }))
-  .then((dbArticle) => {
-    res.json(dbArticle);
-  })
-  .catch((err) => {
-    res.json(err);
-  })
-});
-
-app.delete("/articles/:id" , (req, res) => {
-
-    db.Note.remove().then((dbNote) => {
-      res.json(dbNote);
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
     })
+  );
+  app.set("view engine", "handlebars");
 
-.catch((err) => {
-  res.json(err);
-})
-});
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // Start the server
-  app.listen(PORT, () => {
-    console.log(`App running on port ${  PORT  }!`);
-  });
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
