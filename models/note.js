547f3cbc4b7bb62123c75f2ba37a5780c1c6fb5d
dyl trimble <dyltrimble12@gmail.com>
2020-01-13 13:08:58 -0500

    cleaned up commenting

--- models/note.js
+++ models/note.js
@@ -1,6 +1,5 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new NoteSchema object
