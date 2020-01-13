21d78a34aee52e78a083f5e4c8aa9849b93dd5be
dyl trimble <dyltrimble12@gmail.com>
2020-01-13 13:10:57 -0500

    cleaned up the save js and made a dryer model

--- models/Save.js
+++ models/Save.js
@@ -1,17 +1,14 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 var SaveSchema = new Schema({
-  // `title` is required and of type String
+
   title: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
+  
   link: {
     type: String,
     required: true
@@ -20,16 +17,13 @@ var SaveSchema = new Schema({
     type: String,
     required: true
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
+
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
   }
 });
 
-// This creates our model from the above schema, using mongoose's model method
 var Save = mongoose.model("Saved", SaveSchema);
 
 // Export the Article model
